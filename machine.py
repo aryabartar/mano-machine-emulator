88cd9b8d6cfa97388a20e4d59641344883b8ffee
Arya Khaligh <a.khaligh@aut.ac.ir>
2018-10-28 16:39:26 +0330

    3 registers remained !

--- machine.py
+++ machine.py
@@ -1,3 +1,8 @@
+def check_length(value, len):
+    if not len(value) == len:
+        raise Exception("No size match")
+
+
 class Memory:
     memory = [0] * 4096
 
@@ -14,6 +19,39 @@ class DataRegister:
     def read(self):
         return self.data
 
-    def write(self, data) :
+    def write(self, data):
         self.data = data
 
+
+# accumulator remained
+
+class InstructionRegister:
+    instruction = ""
+
+    def read(self):
+        return self.instruction
+
+    def write(self, instruction):
+        self.instruction = instruction
+
+
+class AddressRegister:
+    address = ""
+
+    def read(self):
+        return self.address
+
+    def write(self, value):
+        check_length(value, 12)
+
+
+class ProgramCounter:
+    address = ""
+
+    def read(self):
+        if self.address == "":
+            raise Exception("PC is empty!")
+        return self.address
+
+    def write(self, value):
+        self.address = value

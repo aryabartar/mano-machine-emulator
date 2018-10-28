e5b82aca6a2b31ee68e729d1ebf5aaa0a618cad6
Arya Khaligh <a.khaligh@aut.ac.ir>
2018-10-28 18:58:19 +0330

    T2 implemented .

--- main.py
+++ main.py
@@ -25,13 +25,16 @@ def make_bin_lengthy(a, length):
         a = '0' + a
     return a
 
+
 def bin_add(a, b):
     bin_result = bin(int(a, 2) + int(b, 2))
     result = make_bin_lengthy(bin_result[2:], max(len(a), len(b)))
     return result
 
-def bin_to_decimal (binary_int) :
-    return int(binary_int , 2)
+
+def bin_to_decimal(binary_int):
+    return int(binary_int, 2)
+
 
 def main():
     memory = Memory()
@@ -48,11 +51,21 @@ def main():
     while True:
         wait()
         print("____STARTING____")
+        # T0
         AR.write(PC.read())
         print_status(AC, AR, DR, IR, PC, TR)
+        # T1
         PC.write(bin_add('1', PC.read()))
         IR.write(memory.memory[bin_to_decimal(AR.read())])
         print_status(AC, AR, DR, IR, PC, TR)
+        # T2
+        I = IR.read()[0]
+        AR.write(IR.read()[4:16])
+        D = bin_to_decimal(IR.read()[1:4])
+        print_status(AC, AR, DR, IR, PC, TR)
+
 
 main()
 # print(bin_add('0000011', '1'))
+# a = "12345"
+# print(a[1:2])

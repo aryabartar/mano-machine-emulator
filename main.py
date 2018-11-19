34d2336a4d5011fd26def91d9d4d2659dfd6c16e
arya <a.khaligh@aut.ac.ir>
2018-11-19 14:39:40 +0330

    handle_assembly added.

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ def read_from_input():
     assembly_rows = assembly_raw_input.read().split("\n")
     assembly_splitted_rows = []
     for row in assembly_rows:
-        assembly_splitted_rows.append(row.split("\n"))
+        assembly_splitted_rows.append(row.split(" "))
     return assembly_splitted_rows
 
 
-print(read_from_input())
+def handle_assembly(assembly_list):
+    def has_label(row_list):
+        """
+        :param row_list: Gets a row in form of splitted array.
+        :return: True => if has label  | False => if not
+        """
+        if row_list[0][-1] == ",":
+            return True
+        return False
+
+    for item in assembly_list:
+        print(item[0])
+        if not has_label(item) and item[0] == "LDA":
+            print(item)
+
+
+assembly_list = read_from_input()
+handle_assembly(assembly_list)

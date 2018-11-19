45edfa75ce84501622fe9546fe4890b30fcb4d3c
arya <a.khaligh@aut.ac.ir>
2018-11-19 14:48:52 +0330

    get_numner added.^X
    :x
    
    s2

--- main.py
+++ main.py
@@ -7,20 +7,38 @@ def read_from_input():
     return assembly_splitted_rows
 
 
-def handle_assembly(assembly_list):
-    def has_label(row_list):
-        """
-        :param row_list: Gets a row in form of splitted array.
-        :return: True => if has label  | False => if not
-        """
-        if row_list[0][-1] == ",":
-            return True
-        return False
+def has_label(row_list):
+    """
+    :param row_list: Gets a row in form of splitted array.
+    :return: True => if has label  | False => if not
+    """
+    if row_list[0][-1] == ",":
+        return True
+    return False
 
+
+def get_number(row_list):
+    """
+    :param row_list:
+    :return: Decimal value of "DEC" and "HEX" number.
+    """
+    if has_label(row_list):
+        if row_list[1] == "HEX":
+            return int(row_list[2])
+        elif row_list[1] == "DEC":
+            return int(int(row_list[2]))
+    else:
+        if row_list[0] == "HEX":
+            return False
+        elif row_list[0] == "DEC":
+            return int(int(row_list[1]))
+
+
+def handle_assembly(assembly_list):
     for item in assembly_list:
         print(item[0])
         if not has_label(item) and item[0] == "LDA":
-            print(item)
+            number = get_number(item)
 
 
 assembly_list = read_from_input()
